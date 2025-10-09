d97f2900f6cfda03972b87f72a4483259e0eea18
Aaditya Aren <omaren0248@gmail.com>
2025-10-09 13:25:33 +0530

    lock-based_queue updated with templates

--- lock-based_queue.cpp
+++ lock-based_queue.cpp
@@ -1,34 +1,44 @@
 #include <iostream>
 #include <thread>
 #include <queue>
+template <typename T>
 class Queue{
     private:
-    std::queue<int> q;
+    std::queue<T> q;
     std::mutex mtx;
     std::condition_variable cv;
 
     public:
-    void insert(int value){
+    void insert(T value){
         std::lock_guard<std::mutex> mymtx(mtx);
         q.push(value); // can use std::move like 
         // q.push(std::move(value));
         cv.notify_one();
     }
 
-    int pop(){
+    T wait_pop(){
         std::unique_lock<std::mutex> mymtx(mtx);
         // unique lock is used for wait in cvs since unique lock can be transferred between functions and 
         // provide some additional flexibilities like calling unlock and maybe call lock later using std::defer_lock
         while(q.empty()) cv.wait(mymtx); // there can be spurious wakeups therefore while loop nneded to check empty consition again 
-        int val = q.front(); // can use std::move here also
+        // cv.wait(mymtx,[this] {return !q.empty();}); //can also pass like this pedicate in wait function
+        T val = q.front(); // can use std::move here also
         q.pop();
         return val;
+    }
+
+    bool try_pop(T &value){
+        std::lock_guard<std::mutex> mymtx(mtx);
+        if(q.empty()) return false;
+        else{
+            value = std::move(q.front());
+            q.pop();
+            return true;
+        }
     }
 };
 
 int main(){
-    Queue q;
-    
-
+    std::thread producers[5];
     return 0;
 }
